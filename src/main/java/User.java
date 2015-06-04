b653429affa3f93428dcc601a4b4346234c3ecc0
John Schmidt <jschmidt38@gatech.edu>
2015-06-03 20:14:34 -0400

    see if this works

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,16 +5,36 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class User {
     private String username;
+    private String password;
+    private boolean isUser;
 
     public User() {
         System.out.println("Making user");
+        isUser = false;
     }
 
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String name) {
         username = name;
     }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String pass) {
+        password = pass;
+    }
+    public String getIsUser() {
+        if (username.compareTo("Admin") == 1){
+            isUser = true;
+            return "Go Ahead";
+        }
+        else {
+            isUser = false;
+            return "nope";
+        }
+    }
 }
+
