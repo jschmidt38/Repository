9d6416a1dafaf62a031ecef8cec22bdfaa5604e0
Thanh Tran <ttran90@gatech.edu>
2015-07-01 18:09:01 -0400

    Added an enum for major

---
+++ src/main/java/MajorBean.java
@@ -0,0 +1,14 @@
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created by thanh on 7/1/15.
+ */
+@ManagedBean
+@ApplicationScoped
+public class MajorBean {
+
+    public Major[] getMajors() {
+        return Major.values();
+    }
+}
